83d7d4de542cdda624edf54605b3e9e2e4a1f253
Godfrey <godfrey1604@gmail.com>
2023-06-24 21:08:08 +0800

    模擬登入檢查

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { useState, useEffect } from "react";
 import SignIn from "./page/SignIn/SignIn";
 import OverallLayout from "./components/Layout/OverallLayout/OverallLayout";
-const App = () => {
-  return <OverallLayout />;
+
+let App = () => {
+  const wait = <div></div>;
+  let [result, setResult] = useState(<SignIn />);
+  useEffect(() => {
+    const getUser = () => {
+      let user = "";
+      return new Promise((res, rej) => {
+        return setTimeout(() => {
+          user = "apple";
+          res(user);
+        }, 100);
+      });
+    };
+    getUser().then((res) => {
+      if (res) {
+        setResult(<OverallLayout />);
+      } else {
+        setResult(<SignIn />);
+      }
+    });
+  }, []);
+
+  // };
+  // useEffect(async () => {
+  //   const user = await getUser().then((res) => res);
+  //   if (user === 1) {
+  //     setResult(<OverallLayout />);
+  //   } else {
+  //     setResult(<SignIn />);
+  //   }
+  // }, []);
+
+  return result;
 };
+
+// const App = () => {
+//   return <SignIn />;
+// };
 
 export default App;
